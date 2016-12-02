541818b8ce8c1b979b0d27c2cf6b8ba0f899aa0a
riskihajar <riskihajar@gmail.com>
2016-12-02 09:37:38 +0800

    navbar responsive

--- resources/assets/js/modules/_commons/navbar.vue
+++ resources/assets/js/modules/_commons/navbar.vue
@@ -3,19 +3,29 @@
         <el-col :span="5">
             <div class="brand-title">
                 <icon i="apple" type="mdi"></icon>
-                Laravel SPA Starter
+                <span class="dp-lg-screen inline">Laravel SPA Starter</span>
             </div>
         </el-col>
         <el-col :span="17">
             <el-menu class="sm-menu" theme="dark" mode="horizontal" :default-active="active" :router="true">
-                <el-menu-item index="dashboard.index" :route="{name: 'dashboard.index'}"><icon i="home" type="mdi"></icon> Dashboard</el-menu-item>
-                <el-menu-item index="category.index" :route="{name: 'category.index', query: {page: 1}}"><icon i="layers" type="mdi"></icon> Category</el-menu-item>
+                <el-menu-item class="dp-xs-screen" v-for="item in menu" :index="item.route.name" :route="item.route"><icon :i="item.icon.i" :type="item.icon.type"></icon> {{ item.label }}</el-menu-item>
+                <el-submenu index="1" class="dp-xs-screen-low">
+                    <template slot="title">MENU</template>
+                    <el-menu-item v-for="item in menu" :index="item.route.name" :route="item.route"><icon :i="item.icon.i" :type="item.icon.type"></icon> {{ item.label }}</el-menu-item>
+                </el-submenu>
             </el-menu>
         </el-col>
         <el-col :span="2">
             <el-menu class="sm-menu sm-menu-right" theme="dark" mode="horizontal">
                 <el-submenu index="1">
-                    <template slot="title"><icon i="face" type="mdi"></icon> {{ user.name }}</template>
+                    <template slot="title">
+                        <div class="dp-md-screen">
+                            <icon i="face" type="mdi"></icon> {{ user.name }}
+                        </div>
+                        <div class="dp-md-screen-low">
+                            <icon i="more"></icon>
+                        </div>
+                    </template>
                     <el-menu-item index="1-1"><icon i="account-circle" type="mdi"></icon> Profile</el-menu-item>
                     <el-menu-item index="1-2"><icon i="setting"></icon>Setting</el-menu-item>
                     <el-menu-item index="1-3"><a @click.prevent="logout"><icon i="sign-in" type="mdi"></icon> Logout</a></el-menu-item>
@@ -35,6 +45,33 @@ export default{
         ...mapState(['user']),
         active(){
             return this.$router.history.current.name
+        }
+    },
+    data(){
+        return{
+            menu: [
+                {
+                    route:{
+                        name: 'dashboard.index'
+                    },
+                    label: 'Dashboard',
+                    icon: {
+                        type: 'mdi',
+                        i: 'home'
+                    }
+                },
+                {
+                    route: {
+                        name: 'category.index',
+                        query: {page: 1}
+                    },
+                    label: 'Category',
+                    icon: {
+                        type: 'mdi',
+                        i: 'layers'
+                    }
+                }
+            ]
         }
     },
     methods: {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel SPA Starter</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
