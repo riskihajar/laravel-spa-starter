678e3ebbbb8123cfdcefce536ee6e9f402dd300b
riskihajar <riskihajar@gmail.com>
2016-12-02 09:57:27 +0800

    add meta opengraph

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="riskihajar">
+    <meta property="og:title" content="Laravel SPA Starter Kit" />
+    <meta property="og:url" content="{{ request()->fullUrl() }}" />
+    <meta property="og:description" content="Vue.js + Laravel Based SPA Starter Kit" />
+    <meta property="og:image" content="{{ asset('img/cover.png') }}" />
     <title>Laravel SPA Starter</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
